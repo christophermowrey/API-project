de48fe8c1ecbb8487d6dd97f102cd7a34c298416
christophermowrey <christophermowrey@gmail.com>
2022-08-02 11:24:36 -0400

    added nonfunctioning display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,27 +33,38 @@ button.addEventListener("click", function buttonclick(event) {
 
 if (countryOrigin === undefined) {
     var countryOrigin = "usd";
-  } 
-  if (countryDestination === undefined) {
-    var countryDestination = "eur";
-  }
+} 
+if (countryDestination === undefined) {
+ var countryDestination = "eur";
+}
   
   var currencyUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/' + countryOrigin + "/" + countryDestination + '.json';
   // https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@{apiVersion}/{date}/{endpoint}
   // https://github.com/fawazahmed0/currency-api
   
-  var responseText = document.getElementById('response-text');
+//   var responseText = document.getElementById('response-text'); //whatever the html ID is
   
   function getApi(currencyUrl) {
     fetch(currencyUrl)
       .then(function (response) {
         console.log(response);
         return response.json();
+        if (response.ok) {
+            response.json()
+                .then(function (data) {
+                    displayCurrency(data);
+                    // storeCurrency(data);
+                });
+        };
         })
     .then(function (data) {
       console.log(data);
     })
   }
   
+var displayCurrency = function () {
+  var countryCode = document.querySelector("h2");
+  countryCode.textContent = ip.country_code;
+}
+
   getApi(currencyUrl);
-  
